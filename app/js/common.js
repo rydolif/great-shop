54e022e5153eb20b35671eb751faf09e1fb50104
rudolif <rudolifrudolif@gmail.com>
2018-12-19 11:57:55 +0200

    commit

---
+++ app/js/common.js
@@ -0,0 +1,139 @@
+$(function() {
+
+
+//------------------------------гамбургер-----------------------------
+  $('.hamburger').click(function() {
+    $(this).toggleClass('hamburger--active');
+    $('.nav--responsive').toggleClass('nav--active');
+    $('header').toggleClass('header--menu');
+  });
+
+//-------------------------------попандер---------------------------------------
+  $('.modal').popup({transition: 'all 0.3s'});
+
+//------------------------------------form-------------------------------------------
+  $('input[type="tel"]').mask('+0 (000) 000-00-00');
+
+  jQuery.validator.addMethod("phoneno", function(phone_number, element) {
+     return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
+  }, "Введите Ваш телефон");
+
+  $(".form").each(function(index, el) {
+    $(el).addClass('form-' + index);
+
+    $('.form-' + index).validate({
+      rules: {
+        phone: {
+          required: true,
+          phoneno: true
+        },
+        name: 'required',
+      },
+      messages: {
+        name: "Введите Ваше имя",
+        phone: "Введите Ваш телефон",
+      },
+      submitHandler: function(form) {
+        var t = {
+          name: jQuery('.form-' + index).find("input[name=name]").val(),
+          phone: jQuery('.form-' + index).find("input[name=phone]").val(),
+          subject: jQuery('.form-' + index).find("input[name=subject]").val()
+        };
+        ajaxSend('.form-' + index, t);
+      }
+    });
+
+  });
+
+  function ajaxSend(formName, data) {
+    jQuery.ajax({
+      type: "POST",
+      url: "sendmail.php",
+      data: data,
+      success: function() {
+        $(".modal").popup("hide");
+        $("#thanks").popup("show");
+        setTimeout(function() {
+          $(formName).trigger('reset');
+        }, 2000);
+      }
+    });
+  }
+
+// //----------------------------------------fixed----------------------------------
+//   $(window).scroll(function(){
+//       if($(this).scrollTop()>20){
+//           $('.header').addClass('header--active');
+//       }
+//       else if ($(this).scrollTop()<20){
+//           $('.header').removeClass('header--active');
+//       }
+//   });
+
+//-------------------------скорость якоря---------------------------------------
+  $(".header__list").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
+  //--------------------закриття меню при кліку на ссилку якоря--------------------
+     // $('.hamburger').removeClass('hamburger--active');
+     // $('.header-menu').removeClass('header-menu');
+     // $('.header--active').removeClass('header--active');
+     // $('.nav--active').removeClass('nav--active');
+
+  });
+ 
+});
+
+//----------------------------------------preloader----------------------------------
+
+  $(window).on('load', function(){
+    $('.preloader').delay(1000).fadeOut('slow');
+  });
+
+
+//-------------------slider---------------
+
+  $('.banner__slider').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+  });
+
+  $('.bestsellers__slider').slick({
+    infinite: true,
+    dots: false,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+   ]
+  });
+
+
